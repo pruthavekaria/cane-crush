d2f19e5867af05614f4fe059f44bb3dc3ee6bfeb
Prutha14 <nisit.patel@softqubes.com>
2024-06-06 19:32:14 +0530

    added files

--- cane_crush/views.py
+++ cane_crush/views.py
@@ -13,8 +13,10 @@ from django.conf import settings
 def home(request):
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def products(request):
     products = Product.objects.all()
@@ -28,8 +30,22 @@ def products(request):
 
 def product_view(request, slug):
     if request.method == "POST":
+        selected_product_name = request.POST.get('selected_product_name')
+        selected_discounted_price = request.POST.get('selected_discounted_price')
+        selected_original_price = request.POST.get('selected_original_price')
         selected_pack_size = request.POST.get('selected_pack_size')
+        print(selected_discounted_price)
+        print(selected_original_price)
         print(selected_pack_size)
+        print(selected_product_name)
+        product = Product.objects.get(name=selected_product_name)
+
+        order = Order.objects.create(customer=request.user, paid=False)
+        if selected_discounted_price:
+            OrderItem.objects.create(order=order, product=product, quantity=1, price=selected_discounted_price)
+        else:
+            OrderItem.objects.create(order=order, product=product, quantity=1, price=selected_original_price)
+        return redirect('cart')
 
     product = get_object_or_404(Product, slug=slug)
     pack_sizes = product.pack_size.all()
@@ -46,17 +62,28 @@ def product_view(request, slug):
         'pack_sizes': pack_sizes,
         'ordered_pack_sizes': ordered_pack_sizes
     }
-    print("context :-", context)
+
     return render(request, 'product-view.html', context)
 
 
 def view_cart(request):
-    cart_items = OrderItem.objects.all()
-    total_amount = sum(item.total_price for item in cart_items)
+    orders = Order.objects.filter(customer=request.user)
+    cart_items = OrderItem.objects.filter(order__in=orders)
+
+    total_amount = sum(item.price * item.quantity for item in cart_items)
     context = {
-        'cart_items': cart_items, 'total_amount': total_amount}
+        'cart_items': cart_items,
+        'total_amount': total_amount
+    }
 
     return render(request, 'cart.html', context)
+
+
+def remove_item_from_cart(request, id):
+    cart_item = get_object_or_404(OrderItem, id=id)
+    cart_item.delete()
+    return redirect('cart')
+
 
 def contact_us(request):
     if request.method == 'POST':
@@ -66,10 +93,13 @@ def contact_us(request):
             subject = f"New contact message from {form.cleaned_data['fname']} {form.cleaned_data['lname']}"
             message = f"From: {form.cleaned_data['fname']} {form.cleaned_data['lname']} <{form.cleaned_data['email']}>\n\nMessage:\n{form.cleaned_data['message']}"
             sender_email = form.cleaned_data['email']
-            recipient_list = ['mailto:axayqa1221@gmail.com']
+            recipient_list = ['axayqa1221@gmail.com']
             send_mail(subject, message, sender_email, recipient_list)
             return redirect('home')
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form': form})
 
+
+def blog(request):
+    return render(request, 'blog.html')
